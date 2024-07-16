8d9e09f9c301b088f9c1564742a0b97a2936a60f
rfrenchy <ryanfrench@outlook.com>
2024-07-16 15:11:45 +0100

    save as jpeg

--- remove-yellow.py
+++ remove-yellow.py
@@ -7,22 +7,23 @@ def remove_yellow_from_pdf(pdf_path, output_path):
     pdf = fitz.open(pdf_path)
 
     # iterate over each pdf page
-    for i in range(pdf.page_count): # should only be 2 pages
-        page = pdf.load_page(i)
-        pixmap = page.get_pixmap()
+    page = pdf.load_page(0)
+    pixmap = page.get_pixmap()
 
-        # iterate over each pixel in the page
-        for y in range(pixmap.height):
-            for x in range(pixmap.width):
-                r, g, b = pixmap.pixel(x, y)
-                if r > 200 and g > 200 and b < 100:         # if YELLOW
-                    pixmap.set_pixel(x, y, (255, 255, 255)) # set to WHITE
+    # iterate over each pixel in the page
+    for y in range(pixmap.height):
+        for x in range(pixmap.width):
+            r, g, b = pixmap.pixel(x, y)
+            if r > 200 and g > 200 and b < 100:         # if YELLOW
+                pixmap.set_pixel(x, y, (255, 255, 255)) # set to WHITE
 
-        # update the page with the modified pixmap i.e. without yellow
-        page.set_pixmap(pixmap)
-
+    # update the page with the modified pixmap i.e. without yellow
+    # page.set_pixmap(pixmap) # (method doesn't exist)
+    
     # save the modified pdf
-    pdf.save(output_path)
+    pixmap.save(output_path)
+    
+    # pdf.save(output_path)
     pdf.close()
 
 # Usage: python remove-yellow-otta.py otta-cv.pdf otta-cv-no-yellow.pdf
