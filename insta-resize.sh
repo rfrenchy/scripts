4db83184f08aa329a5aa6e25e86b2302bd088c43
Ryan French <ryanfrench@outlook.com>
2024-10-19 18:35:22 +0100

    last

--- insta-resize.sh
+++ insta-resize.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
 # dependencies: imagemagick - sudo apt install imagemagick
+# usage: ./insta-resize.sh image-name.png
 
-# ${1%.*} TODO
+# Instagram defined image sizes
+SQUARE="1080x1080"
+LANDSCAPE="1080x566"
+PORTRAIT="1080x1350"
 
+# Name of the image thats been input i.e. name without the file extension
+IMAGE_NAME=${1%.*}
 
+wx=$(identify $1 | awk '{print $3}' | cut -f 1 -d x) # width of original image 
+hx=$(identify $1 | awk '{print $3}' | cut -f 2 -d x) # height of original image 
 
-IMAGE_NAME=$(echo $1 | cut -f 1 -d .)
+# 1:1
+square () {
+    # find the biggest of the two
+    x=$(($1 > $2 ? $1 : $2))
+    
+    # return new dimensions
+    echo "${x}x${x}"
+    
+    # TODO return image from function with image magick
+    # e.g. echo $(convert -size $(square $dx) xc: white-composite.png)
+}
 
-# get and compare both dimensions and store biggest
-wxh=$(identify $1 | awk '{print $3}')
+# 1:1.91
+landscape () {
+    # have min height of original image
+    # make width 1.91 : of that
+    # or add to height to make 1.19
+    echo ((1.91 * $2))
+}
 
-w=$(echo $wxh | cut -f 1 -d x)
-h=$(echo $wxh | cut -f 2 -d x)
-x=$((w > h ? w : h))
+echo $(landscape $wx $hx)
+
+# 4:5
+portrait () {
+    echo ""
+
+}
 
 # create temp white image and composite
-convert -size ${x}x${x} xc: white-composite.png
+convert -size $(square $wx $hx) xc: white-composite.png
 composite {$1} white-composite.png ${IMAGE_NAME}-comp.png
 
-# scale to instagram size (1024x1024)
-convert ${IMAGE_NAME}-comp.png -resize 1024x1024 ${IMAGE_NAME}-insta-1x1.png
+# TODO get difference between biggest and smallest dimension
+# shift original image 1/2 in that direction on composite
 
+# scale to instagram sizes
+convert ${IMAGE_NAME}-comp.png    -resize ${SQUARE}    ${IMAGE_NAME}-insta-square.png
+# convert ${IMAGE_NAME}-landscape.png -resize ${LANDSCAPE} ${IMAGE_NAME}-insta-landscape.png
+# convert ${IMAGE_NAME}-portrait.png  -resize ${PORTRAIT}  ${IMAGE_NAME}-insta-portrait.png
+
+# split by word for each line. at each word index for each line, find max word length at index, and fill all the others with spaces to max word length for that index
+
+# remove temp white image
+# rm ${IMAGE_NAME}-comp.png
