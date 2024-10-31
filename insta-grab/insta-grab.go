e73dcfcb3126d8f0fea16fecc5c4bf2ad878534e
rfrenchy <ryanfrench@outlook.com>
2024-10-31 20:07:05 +0000

    debug options to see what chromedp is doing

--- insta-grab/insta-grab.go
+++ insta-grab/insta-grab.go
@@ -15,32 +15,38 @@ func main() {
 		Usage: "use to download images from a given instagram url",
 		Action: func(ctx *cli.Context) error {
 
-			ctxx, cancel := chromedp.NewContext(ctx.Context)
-			defer cancel()
-
-			// fmt.Println("hello world")
-
 			var ix string
-			username := os.Getenv("INSTAGRAM_USERNAME")
-			password := os.Getenv("INSTAGRAM_PASSWORD")
+			//			username := os.Getenv("INSTAGRAM_USERNAME")
+			//	password := os.Getenv("INSTAGRAM_PASSWORD")
 
-			fmt.Println(username)
+			//			fmt.Println(username)
 
 			// TODO, setup a profile directory to avoid logging in out
 			// https://stackoverflow.com/questions/59287313/load-up-existing-chrome-user-profile-data-with-chromedp
-			chromedp.Flag("profile-directory", "Profile 1")
-			chromedp.UserDataDir("/home/user/.config/chromium/Profile 1")
-			chromedp.Flag("disable-sync", false)
+
+			opts := append(chromedp.DefaultExecAllocatorOptions[:],
+				chromedp.Flag("profile-directory", "Profile 1"),
+				chromedp.UserDataDir("/home/ryan/.config/google-chrome/Profile 1"),
+				chromedp.Flag("disable-sync", false),
+				chromedp.Flag("headless", false),
+			)
+
+			e_ctx, cancel := chromedp.NewExecAllocator(ctx.Context, opts...)
+			defer cancel()
+
+			ctxx, cancel := chromedp.NewContext(e_ctx, chromedp.WithLogf(log.Printf))
+			defer cancel()
 
 			// login
 			err := chromedp.Run(ctxx,
-				chromedp.Navigate("https://www.instagram.com/p/C04EDoCIahN/"),           // go to url
-				chromedp.Click("._a9--", chromedp.ByQuery),                              // click allow all cookies
-				chromedp.SendKeys("[aria-label~=username]", username, chromedp.ByQuery), // enter username
-				chromedp.SendKeys("[aria-label=Password]", password, chromedp.ByQuery),  // enter password
-				chromedp.OuterHTML("button._acan", &ix, chromedp.ByQuery),               // click login
-				//chromedp.OuterHTML("[aria-label=Password]", &ix, chromedp.ByQuery),      // click login
-				//chromedp.OuterHTML("*", &ix, chromedp.ByQuery),
+				chromedp.Navigate("https://www.instagram.com/p/C04EDoCIahN/"), // go to url
+				chromedp.OuterHTML("img[src*=scontent]", &ix, chromedp.ByQuery),
+			//	chromedp.Click("._a9--", chromedp.ByQuery),                              // click allow all cookies
+			//	chromedp.SendKeys("[aria-label~=username]", username, chromedp.ByQuery), // enter username
+			//	chromedp.SendKeys("[aria-label=Password]", password, chromedp.ByQuery),  // enter password
+			//	chromedp.OuterHTML("button._acan", &ix, chromedp.ByQuery),               // click login
+			//chromedp.OuterHTML("[aria-label=Password]", &ix, chromedp.ByQuery),      // click login
+			//chromedp.OuterHTML("*", &ix, chromedp.ByQuery),
 			)
 
 			if err != nil {
