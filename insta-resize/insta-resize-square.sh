08c9eaf49e847d69552b6a615513be424821e36a
Ryan French <ryanfrench@outlook.com>
2024-11-03 18:06:09 +0000

    flip go off, consolidate bash

--- insta-resize/insta-resize-square.sh
+++ insta-resize/insta-resize-square.sh
@@ -2,22 +2,44 @@
 # dependencies: imagemagick - sudo apt install imagemagick
 # usage: ./insta-resize.sh image-name.png
 
-# get original height and width
-wx=$(identify "$1" | awk '{print $3}' | cut -f 1 -d x) 
-hx=$(identify "$1" | awk '{print $3}' | cut -f 2 -d x)
+square() {
+    # Get original height and width
+    wx=$(identify "$1" | awk '{print $3}' | cut -f 1 -d x) 
+    hx=$(identify "$1" | awk '{print $3}' | cut -f 2 -d x)
 
-# find the biggest of the two
-dx=$((wx > hx ? wx : hx))
-# 1:1 dimension string
-dxx="${dx}x${dx}" 
+    # Find biggest dimension of the two, 
+    # then use to make 1:1 dimension string
+    dx=$((wx > hx ? wx : hx))                             
+    dxx="${dx}x${dx}"                                     
 
-IMAGE_OUTPUT_NAME=${1%.*} # Name of the image thats been input i.e. name without the file extension
+    # Use the name of the image thats been input, minus the file extension 
+    # e.g. my-picture.png becomes my-picture
+    IMAGE_OUTPUT_NAME=${1%.*} 
 
-# create temp white image and composite
-convert -size $dxx xc: white-composite.png
-composite "$1" white-composite.png "$IMAGE_OUTPUT_NAME"-comp.png
+    # Create a white image at the new calculated dimension and 
+    # composite with the original image
+    convert -size $dxx xc: white-composite.png
+    composite "$1" white-composite.png "$IMAGE_OUTPUT_NAME"-comp.png
 
-# scale to instagram sizes
-convert "$IMAGE_OUTPUT_NAME"-comp.png -resize "1080x1080" "$IMAGE_OUTPUT_NAME"-insta-square.png
+    # Scale composited image to instagram's square measurements
+    convert "$IMAGE_OUTPUT_NAME"-comp.png -resize "1080x1080" "$IMAGE_OUTPUT_NAME"-insta-square.png
 
-# MISC split by word for each line. at each word index for each line, find max word length at index, and fill all the others with spaces to max word length for that index
+    # MISC split by word for each line. at each word index for each line, find max word length at index, and fill all the others with spaces to max word length for that index
+}
+
+portrait() {
+    echo "portrait"
+}
+
+landscape() {
+    echo "landscape"    
+}
+
+case "$1" in 
+    square) square "$2" ;;
+    portrait) portrait "$2" ;;
+    landscape) landscape "$2" ;;
+esac
+
+
+
