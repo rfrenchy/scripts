d1e3669ceaee01550b08c5517b0500deb643161d
Ryan French <ryanfrench@outlook.com>
2024-11-06 12:18:20 +0000

    some lua

--- insta-resize/insta-resize.lua
+++ insta-resize/insta-resize.lua
@@ -1,23 +1,35 @@
 
 
--- Resize image to 1080x1080 (1:1)
-function Square ()
-    return "square mode"
-end
+-- object for creating a square image for instagram
+local square = {
+    -- return a 1:1 dimension string of image
+    dimension = function (height, width)
+        -- use whatever measurement is biggest  
+        local d = height > width and height or width;
+        -- create 1:1 string of biggest measurement e.g. 1230x1230
+        return string.gsub("%sx%s", "%s", d)
+    end,
 
--- Resize image to 1080x1350 (4:5)
-function Portrait()
-    print("portrait mode")
-end
+    -- return instagram specific 1:1 measurement 
+    instasize = function () return "1080x1080" end
+}
 
--- Resize image to 1080x566 (1.91:1)
-function Landscape()
-    print("landscape")
-end
+-- object for creating portrait sized images for instagram
+local portrait = {
+    -- return a 4:5 dimension string of image
+    dimension = function (height, width)   
+        local wx = (height / 5) * 4 -- new width
+        return string.gsub("%sx" .. height, "%s", wx)
+    end,
+    -- return instagram specific portrait size 
+    instasize = function () return "1080x1350" end
+}
 
-main = {}
-main["square"] = Square()
-main["portrait"] = Portrait
-main["landscape"] = Landscape
+local landscape = {
+    dimension = function () return "" end,
+    instasize = function () return "" end
+}
+
+local main = {}
 
 print(main[arg[1]])
