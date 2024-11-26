60763df87f6dc6085d0c5fafa3b4a992e01fdc1e
rfrenchy <ryanfrench@outlook.com>
2024-11-26 12:39:44 +0000

    in twos

--- animate/animate.py
+++ animate/animate.py
@@ -1,6 +1,6 @@
 
 # usage: python3 animate.py directory-name
-# description: stitch together images in a directory into a gif at 24 FPS
+# description: stitch together images in a directory into a gif
 import numpy as np
 import argparse
 import glob
@@ -13,8 +13,8 @@ argp.add_argument("directory")
 
 args = argp.parse_args()
 
+# images: tuple[Any, ImageFile] = []
 images = []
-
 # get all images in given directory
 for f in glob.glob(args.directory + "/*.jpg"):
     images.append((f, Image.open(f)))
@@ -27,9 +27,29 @@ images = []
 for i in sorted_images:
     images.append(i[1])
 
+in_twos = []
+for i in images:
+    in_twos.append(i)
+    in_twos.append(i)
+
+in_twos[0].save("arm-in-twos.gif",
+                save_all=True,
+                append_images=in_twos[1:],
+                duration=1,
+                loop=0)
+
+# todo, make 24 fps?
+
 # make a jif
-images[0].save("arm-animation.gif",
-               save_all=True,
-               append_images=images[1:],
-               duration=1,
-               loop=0)
+# images[0].save("arm-animation.gif",
+#               save_all=True,
+#               append_images=images[1:],
+#               duration=1,
+#               loop=0)
+
+# 24 / 9 = 2.666
+#
+
+
+# arm specific settings
+# inbetween in halfs?
