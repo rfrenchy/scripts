84737aedae2c26c520c143033be3681eaa1059f9
rfrenchy <ryanfrench@outlook.com>
2024-12-03 17:05:18 +0000

    second pass

--- parabola/parabola.py
+++ parabola/parabola.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # command line argument set up
 argp = argparse.ArgumentParser("parabola")
 
-argp.add_argument("--pow", default=1)
+argp.add_argument("--pow", default=1, type=int)
 argp.add_argument("-o", "--output", default="ball.png")
 args = argp.parse_args()
 
@@ -20,7 +20,12 @@ plt.ylabel("distance-fallen")
 y_start = np.array([0, -0.1, -0.19, -0.3, -0.575, -0.825, -1.225,
                     -1.650, -2.19, -2.75, -3.41, -4.125, -4.825])
 
-y_final = np.negative(np.power(y_start, args.pow))
+# user operations
+y_pow = np.power(y_start, args.pow)
+
+# make sure y-data remain negative no matter pow value
+y_pow = -y_pow if args.pow % 2 == 0 else y_pow
+y_final = y_pow
 
 # plot
 plt.scatter(x, y_final)
---
+++ parabola/run.sh
@@ -0,0 +1,9 @@
+python3 parabola.py                       
+python3 parabola.py --pow 2 -o ball-p2.png
+python3 parabola.py --pow 3 -o ball-p3.png
+python3 parabola.py --pow 4 -o ball-p4.png
+python3 parabola.py --pow 5 -o ball-p5.png
+
+find . -maxdepth 1 -name "ball*" |
+    xargs -n1 xdg-open 
+
