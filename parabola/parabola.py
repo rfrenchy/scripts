c56ebb0a5ecfcfcfeca95406e146ccc6a2733902
rfrenchy <ryanfrench@outlook.com>
2024-12-04 14:30:29 +0000

    as gif

--- parabola/parabola.py
+++ parabola/parabola.py
@@ -4,6 +4,7 @@ import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # command line argument set up
 argp = argparse.ArgumentParser("parabola")
@@ -12,11 +13,11 @@ argp.add_argument("--pow", default=1, type=int)
 argp.add_argument("-o", "--output", default="ball.png")
 args = argp.parse_args()
 
-# axis data
-plt.xlabel("frames")
+# # axis data
+# plt.xlabel("frames")
 x = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24])
 
-plt.ylabel("distance-fallen")
+# plt.ylabel("distance-fallen")
 y_start = np.array([0, -0.1, -0.19, -0.3, -0.575, -0.825, -1.225,
                     -1.650, -2.19, -2.75, -3.41, -4.125, -4.825])
 
@@ -27,7 +28,29 @@ y_pow = np.power(y_start, args.pow)
 y_pow = -y_pow if args.pow % 2 == 0 else y_pow
 y_final = y_pow
 
+fig, ax = plt.subplots()
+ax.set_xlim([0, 24])
+ax.set_ylim([-5, 5])
+
+scat = ax.scatter(1, 0)
+
+
+def animate(i):
+    scat.set_offsets((x[i], y_final[i]))
+    return scat,
+
+
+ani = animation.FuncAnimation(fig, animate, repeat=True,
+                              frames=len(x) - 1, interval=50)
+
+writer = animation.PillowWriter(fps=12, metadata=dict(artist="ry"),
+                                bitrate=1800)
+
+ani.save('scatter.gif', writer=writer)
+
+# plt.show()
+
 # plot
-plt.scatter(x, y_final)
-plt.grid()
-plt.savefig(args.output)
+# plt.scatter(x, y_final)
+# plt.grid()
+# plt.savefig(args.output)
--- parabola/run.sh
+++ parabola/run.sh
@@ -1,9 +1,11 @@
 python3 parabola.py                       
-python3 parabola.py --pow 2 -o ball-p2.png
-python3 parabola.py --pow 3 -o ball-p3.png
-python3 parabola.py --pow 4 -o ball-p4.png
-python3 parabola.py --pow 5 -o ball-p5.png
+xdg-open scatter.gif
 
-find . -maxdepth 1 -name "ball*" |
-    xargs -n1 xdg-open 
+# python3 parabola.py --pow 2 -o ball-p2.png
+# python3 parabola.py --pow 3 -o ball-p3.png
+# python3 parabola.py --pow 4 -o ball-p4.png
+# python3 parabola.py --pow 5 -o ball-p5.png
+# 
+# find . -maxdepth 1 -name "ball*" |
+#     xargs -n1 xdg-open 
 
