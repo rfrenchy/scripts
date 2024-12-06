595b81cf21753c5ce33d1f83ea48511a3733422e
rfrenchy <ryanfrench@outlook.com>
2024-12-06 14:50:08 +0000

    animated overlap

---
+++ animate/timing/overlapping/overlapping.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import math
+
+
+def overlap(frames=14):
+    # build plot
+    fig, ax = plt.subplots()
+
+    math.ceil(frames / 2)
+
+    # movement data points (along y-axis)
+    mv = np.append(np.linspace(0, 1, math.ceil(frames / 2)),
+                   np.linspace(1, 0, math.ceil(frames / 2)))
+
+    # initial data points
+    x = np.array([0, 1, 2, 3, 4])
+
+    # graph visual adjustments
+    ax.spines.get("top").set_visible(False)
+    ax.spines.get("right").set_visible(False)
+
+    # plot movement of each ball for each frame
+    def animate(i):
+        # clear previous animated data on plot
+        ax.cla()
+        ax.set_xlim([-1, 5])
+        ax.set_ylim([0, 1.5])
+
+        c = (i + 4) % frames  # c-ball's current position
+        e = (i + 5) % frames  # e-ball's current position
+        r = (i + 6) % frames  # the rest of the balls current position
+
+        # calculate new y positions for each ball based on current frame
+        y = np.array([mv[r], mv[r], mv[c], mv[r], mv[e]])
+
+        # new plotted data
+        return ax.scatter(x, y)
+
+    # animation function for matplotlib
+    anim = animation.FuncAnimation(fig, animate, repeat=True,
+                                   frames=(frames - 1), interval=100)
+
+    return (fig, anim)
