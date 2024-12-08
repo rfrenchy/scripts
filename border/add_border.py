579c4bae6ef6d015c8d2dd762e6fefbc9f86d590
rfrenchy <ryanfrench@outlook.com>
2024-12-08 09:47:35 +0000

    border script changes

--- border/add_border.py
+++ border/add_border.py
@@ -7,10 +7,12 @@ from PIL import Image, ImageOps
 # command line argument set up
 argp = argparse.ArgumentParser("add_border")
 argp.add_argument(
-    "image_url", "-i", help="the url of the image you want to add a border to", type=str)
+    "-i", "--input", help="the url of the image you want to add a border to", type=str)
+argp.add_argument("-o", "--output", help="name of output", type=str)
 
 WHITE = np.array([255, 255, 255])
 
+argp.add_argument("--size", help="border size to apply", default=25, type=int)
 argp.add_argument("-gs", "--gradientstart", default=WHITE)
 argp.add_argument("-ge", "--gradientend", default=WHITE)
 argp.add_argument("-st", "--steps", default=1)
@@ -18,39 +20,41 @@ argp.add_argument("-st", "--steps", default=1)
 args = argp.parse_args()
 
 # open the image for processing
-img = Image.open(args.image_url)
+img = Image.open(args.input)
 
 # start and end colours for border gradient
 # lambent = np.array([165, 229, 255])
 # storm = np.array([0, 71, 100])
 
 # number of interpolations
-N = 50
+N = args.steps
 
 # check if need to interpolate color
-change_color = (args.gradientstart == args.gradientend).all()
+same_color = (args.gradientstart == args.gradientend).all()
 
-print(change_color)
+cx = WHITE
 
 for i in range(N):
     # get interpolated color
-    if change_color:
+    if not same_color:
         cx = args.gradientend + (i / (N - 1)) * \
             (args.gradientend - args.gradientstart)
 
     x = np.rint(cx).astype(int)
+
+    # RGB of border color fill
     fill = (x[0], x[1], x[2])
 
     # might need to change to pad
 
     border_img = ImageOps.expand(img,
-                                 border=1,
+                                 border=args.size,
                                  fill=fill)
 
     img = border_img
 
 
-border_img.save("bordered_" + args.image_url)
+border_img.save(args.output)
 
 
 # use numpy to add border
---
+++ border/run.sh
@@ -0,0 +1,2 @@
+
+python3 add_border.py -i ./20241125_104908.jpg --size 99 -o bordered.jpg
