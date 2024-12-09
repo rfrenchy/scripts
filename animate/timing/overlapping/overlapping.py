a9e0e3ac600b45521cbeaeabe59c0049d2bede21
rfrenchy <ryanfrench@outlook.com>
2024-12-09 18:13:51 +0000

    3d overlap graphic

--- animate/timing/overlapping/overlapping.py
+++ animate/timing/overlapping/overlapping.py
@@ -7,45 +7,42 @@ import math
 
 def overlap(frames=14):
     # build plot
-    fig, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
 
-    # https://matplotlib.org/stable/gallery/mplot3d/2dcollections3d.html#sphx-glr-gallery-mplot3d-2dcollections3d-py
-    # fig.add_subplot(projection="3d")
-
-    math.ceil(frames / 2)
-
-    # movement data points (along y-axis)
+    # movement data points
+    # TODO slow-in-slow-out
     mv = np.append(np.linspace(0, 1, math.ceil(frames / 2)),
                    np.linspace(1, 0, math.ceil(frames / 2)))
 
-    # TODO slow-in-slow-out
-
     # initial data points
     x = np.array([0, 1, 2, 3, 4])
+    y = np.zeros(5)
 
-    # graph visual adjustments
-    ax.spines.get("top").set_visible(False)
-    ax.spines.get("right").set_visible(False)
-
-    # plot movement of each ball for each frame
+    # plot movement of balls for each frame
     def animate(i):
         # clear previous animated data on plot
         ax.cla()
+
+        # reset axis data
         ax.set_xlim([-1, 5])
-        ax.set_ylim([0, 1.5])
+        ax.set_ylim([-1, 1])
+        ax.set_zlim([0, 1])
+
+        ax.set_xticklabels(["", "A", "B", "C", "D", "E", ""])
+        # ax.set_yticks([])
+        ax.set_yticklabels([])
+        # ax.set_zticks([])
 
         c = (i + 4) % frames  # c-ball's current position
         e = (i + 5) % frames  # e-ball's current position
         r = (i + 6) % frames  # the rest of the balls current position
 
         # calculate new y positions for each ball based on current frame
-        y = np.array([mv[r], mv[r], mv[c], mv[r], mv[e]])
-
-        z = np.zeros(5)
+        z = np.array([mv[r], mv[r], mv[c], mv[r], mv[e]])
 
         # new plotted data
-        return ax.scatter(x, y)
-        # return ax.scatter(x, y, z)
+        return ax.scatter(x, y, z, zdir="z")
 
     # animation function for matplotlib
     anim = animation.FuncAnimation(fig, animate, repeat=True,
