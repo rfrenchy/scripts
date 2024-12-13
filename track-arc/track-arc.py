abb538b8b16967f099b2f576d31d897e1189a6ed
rfrenchy <ryanfrench@outlook.com>
2024-12-13 14:19:24 +0000

    track-arc opencv hello world

---
+++ track-arc/track-arc.py
@@ -0,0 +1,22 @@
+
+# A
+# usage: python3 track-arc.py -i dancer-1000.jpg
+
+import argparse
+
+import cv2 as cv
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+
+
+argp = argparse.ArgumentParser("track-arc")
+argp.add_argument("-i", "--input", default="./daffodil/1265.png")
+argp.add_argument("-o", "--output", default="test.jpg")
+args = argp.parse_args()
+
+img = cv.imread(args.input)
+
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
