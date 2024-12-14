3b19c7b32506e2245199da0e9c21006004ffa09f
rfrenchy <ryanfrench@outlook.com>
2024-12-14 18:42:35 +0000

    clean up

--- tracking/tracking.py
+++ tracking/tracking.py
@@ -7,66 +7,57 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 argp = argparse.ArgumentParser("tracking", 
-                               description="3 projectile path rotated from central point")
+                               description="simulate tracking a player with a projectile")
 argp.add_argument("-i", "--input", default="")
-argp.add_argument("-o", "--output", default="")
+argp.add_argument("-o", "--output", default="tracking.gif")
 
 args = argp.parse_args()
 
 fig, ax = plt.subplots()
 
-# plot/scatter a moving figure moving along x axis, y 0
-# generate random point within area (1 > y > 0.5), x whereever
+# total frames to animate
+frms = 24
 
-frames = 24
+# player simulated coordatinates
+# go smoothly across left to right on y = 0
+plyr_x = np.linspace(0, 1, frms)
+plyr_y = np.zeros(frms)
 
-# all coords
-player = {
-    "x": np.linspace(0.1, 0.9, frames),
-    "y": np.zeros(frames)
-}
+# generate projectile initial coords
+# project start coords (could use np.random.uniform(.5,1) but o well)
+prj_start_x = np.random.rand()                     # 1 > x > 0
+prj_start_y = np.random.rand() * (1 - 0.5) + 0.5   # 1 > y > 0.5
 
-# random y coord in top half of unit square 
-
-
-# generate initial coords
-# np.random.uniform(0.5, 1)
-projectile = {
-    "x": [np.random.rand()],                    # 1 > x > 0
-    "y": [np.random.rand() * (1 - 0.5) + 0.5]   # 1 > y > 0.5
-}
-
-tracking_frame = int(frames / 8)
+# the frame the projectile will start tracking the player from
+track_frame = int(frms / 8)
 
 # do nothing up to tracking frame
-p1x = np.repeat(projectile.get("x")[0], tracking_frame)
-p1y = np.repeat(projectile.get("y")[0], tracking_frame)
+p1x = np.repeat(prj_start_x, track_frame)
+p1y = np.repeat(prj_start_y, track_frame)
 
+# generate coords based on player position on tracking frame
+p2x = np.linspace(prj_start_x, plyr_x[track_frame], frms - track_frame)
+p2y = np.linspace(prj_start_y, plyr_y[track_frame], frms - track_frame)
 
-# gen last two thrid of frames points based
-# on player position at tracking frame
-p2x = np.linspace(projectile.get("x")[0], player.get("x")[tracking_frame], frames - tracking_frame)
-p2y = np.linspace(projectile.get("y")[0], player.get("y")[tracking_frame], frames - tracking_frame)
-
-track_x = np.concatenate((p1x, p2x))
-track_y = np.concatenate((p1y, p2y))
-
-# use magnitude to 
+# add full frame coords together
+trck_x = np.concatenate((p1x, p2x))
+trck_y = np.concatenate((p1y, p2y))
 
 def animate(i):
     ax.cla()
-    ax.set_xlim([0, 1])
-    ax.set_ylim([-0.25, 1])
+    ax.set_xlim([-0.25, 1.25])
+    ax.set_ylim([-0.25, 1.25])
 
-    ax.scatter(player.get("x")[i], player.get("y")[i])
-    ax.scatter(track_x[i], track_y[i])
+    # plot player and projectile positions
+    ax.scatter(plyr_x[i], plyr_y[i])
+    ax.scatter(trck_x[i], trck_y[i])
 
 anim = animation.FuncAnimation(fig, animate, repeat=True,
-                                   frames=(frames - 1), interval=100)
+                                   frames=(frms - 1), interval=100)
 
 writer = animation.PillowWriter(fps=24, metadata=dict(artist="ry"),
                                 bitrate=1800)
 
-anim.save("tracking.gif", writer=writer)
+anim.save(args.output, writer=writer)
 
 plt.show()
