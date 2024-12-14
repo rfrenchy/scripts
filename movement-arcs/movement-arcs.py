50dc61ef9752d9d056da8430d12d8db833065e4d
rfrenchy <ryanfrench@outlook.com>
2024-12-14 13:54:34 +0000

    one rotation

--- movement-arcs/movement-arcs.py
+++ movement-arcs/movement-arcs.py
@@ -23,13 +23,13 @@ theta    = np.pi / 4
 rotation = np.array([[ np.cos(theta), -np.sin(theta)   ],
                      [ np.sin(theta),  np.cos(theta)   ]])
 a = np.array([1, 1])# circle center
-p = np.array([1, 0])
+p = np.array([[1,1 ],[1, 0]])
 
 # p = {"x": [a.x, a.y], "y": [a.x, a.y - 1] } # line from circle center
 # r = (p.x - a.x)**2 + (p.y - a.y) # radius of circle
 
 p_prime   = p - a
-p_rotated = rotation * p_prime
+p_rotated = rotation @ p_prime
 
 final_rotated = p_rotated + a 
 
@@ -37,19 +37,29 @@ circle  = plt.Circle(a, 1,
                      color='gray', 
                      fill=False, 
                      linestyle='dashed')
+
 fig, ax = plt.subplots()
 
-ax.add_artist(circle)
-plt.quiver(a[0], a[1], p[0] - a[0], p[1] - a[1], 
-            angles='xy',
-            scale_units='xy',
-            scale='1',
-            color='r'
-           )
+# ax.add_artist(circle)
 
+# plt.quiver(a[0], a[1], p[0] - a[0], p[1] - a[1], 
+#             angles='xy',
+#             scale_units='xy',
+#             scale='1',
+#             color='r'
+#            )
+
+ax.add_patch(circle)
+
+print(p)
+print(p_rotated)
+
+ax.scatter(a[0], a[1])
+ax.plot(p[0], p[1])
+ax.plot(final_rotated[0], final_rotated[1])
 
 plt.axis('equal')
-# plt.show()
+plt.show()
 # fig.savefig("test.jpg")
 
 # rotation transform?
