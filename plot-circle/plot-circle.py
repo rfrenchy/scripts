09b8eba13f2470342ff5d8c4ca178ed7c968aee3
rfrenchy <ryanfrench@outlook.com>
2024-12-17 10:20:09 +0000

    growing circle animation

--- plot-circle/plot-circle.py
+++ plot-circle/plot-circle.py
@@ -3,44 +3,58 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # build plot
-fig = plt.figure()
-# fig.set_tight_layout(True)
-ax = fig.add_subplot(projection="3d")
-# ax = fig.add_subplot()
+fig, ax = plt.subplots()
 
-frames = 1
+frames = 24
 
-# given circles center point
-# measure out from center to an edge = D
-# then circle formula to find the rest of the points?
-
-# ax.fill_between()
-
-c = plt.Circle((0, 0), radius=5, color="purple")
-
-# area = pi * (r * r)
-
-# TODO
-# https://stackoverflow.com/questions/56870675/how-to-do-a-3d-circle-in-matplotlib
+def rotation(theta):
+    return np.array([[ np.cos(theta), -np.sin(theta)   ],
+                     [ np.sin(theta),  np.cos(theta)   ]])
 
 
-def animate(i):
-    # clear previous animated data on plot
-    ax.cla()
 
-    # reset axis data
-    ax.set_xlim([-10, 10])
-    ax.set_ylim([-10, 10])
-    ax.set_zlim([-10, 10])
+def growingcircle(scale = 1):
+    a = np.array([0, 0]) # circle center
+    b = np.array([0, 1])
 
-    ax.add_patch(c)
+    # rotation angle (360/8 = 45)
+    rots = 60
+    theta =(2 * np.pi) / rots
+
+    circle_x = np.array([])
+    circle_y = np.array([])
+
+    for i in range(rots):
+        m = rotation(theta * i) @ b
+        circle_x = np.append(circle_x, m[0])
+        circle_y = np.append(circle_y, m[1])
+
+    # last entry to join back up to make full circle
+    circle_x = np.append(circle_x, circle_x[0])
+    circle_y = np.append(circle_y, circle_y[0])
+
+    circle_x = circle_x * scale
+    circle_y = circle_y * scale
+
+    # probably a better way to plot a circle than 
+    # this, curve function?
+
+    
+    ax.plot(circle_x, circle_y)
+
+# growingcircle(ax, frames)
+
+# fig.savefig("circle.jpg")
 
 
-# animation function for matplotlib
-anim = animation.FuncAnimation(fig, animate, repeat=False,
-                               frames=1, interval=100)
+ax.axis("equal")
+ax.set_xlim(-24, 24)
+ax.set_ylim(-24, 24)
+
+anim = animation.FuncAnimation(fig, growingcircle, repeat=False,
+                                frames=24, interval=100)
 
 writer = animation.PillowWriter(fps=12, metadata=dict(artist="ry"),
-                                bitrate=1800)
+                                 bitrate=1800)
 
 anim.save("circle.gif", writer=writer)
