d4952b10df8260c67b10097f6ad177885d16f8b6
rfrenchy <ryanfrench@outlook.com>
2025-01-12 22:49:21 +0000

    working for n < 2 fractal carpet

--- geom/fractals.py
+++ geom/fractals.py
@@ -14,49 +14,46 @@ def assert_carpet_range():
     print("or dont")
 
 # plot carpet segment
-# INCORRECT
+# Fails at n > 2
 def carpet_segment(n, sg = unit_square()):
     # exit recursion
     if n == 0:
         return
 
     sgx, sgy = sg 
+    plt.plot(sgx, sgy, "b")
 
     x2, y2 = (1/c)*sgx, (1/c)*sgy         
-    tr_rng = (np.max(x2) - np.min(x2)) 
-
     ux, uy = unit_square()
 
+    # inefficient - replace
     if np.array_equal(sgx, ux) or np.array_equal(sgy, uy):
-        x1, y1 = sgx - tr_rng, sgy - tr_rng
+        tx = (np.max(x2) - np.min(x2)) 
 
-        # paint
-        plt.plot(x2, y2) # paint inner square
-        plt.plot(x1, y1) # paint inner whtie square
-
-        carpet_segment(n-1, (x2,y2))
+        plt.plot(x2+tx, y2+tx)
+        carpet_segment(n, (x2+tx,y2+tx))
 
     else:
-        plt.plot(sgx, sgy)
+        tx = (np.max(sgx) - np.min(sgx))
+
+        # need to have an origin? to translate from
+
         # (0,0),(1,0),(2,0)
-
-
-
         carpet_segment(n-1, (x2, y2))     
-        carpet_segment(n-1, (x2-tr_rng, y2))
-        carpet_segment(n-1, (x2-(tr_rng*2), y2))
+        carpet_segment(n-1, (x2+tx, y2))
+        carpet_segment(n-1, (x2+(tx*2), y2))
 
         # (0,1),(1,1),(2,1)
-        carpet_segment(n-1, (x2, y2-tr_rng))
-        carpet_segment(n-1, (x2-tr_rng, y2-tr_rng))
-        carpet_segment(n-1, (x2-(tr_rng*2), y2-tr_rng))
+        carpet_segment(n-1, (x2, y2+tx))
+        # carpet_segment(n-1, (x2+trs, y2+trs))
+        carpet_segment(n-1, (x2+(tx*2), y2+tx))
 
         # (0,2),(1,2),(2,2)
-        carpet_segment(n-1, (x2, y2-(tr_rng*2)))      
-        carpet_segment(n-1, (x2-tr_rng, y2-(tr_rng*2)))
-        carpet_segment(n-1, (x2-(tr_rng*2), y2-(tr_rng*2)))
+        carpet_segment(n-1, (x2, y2+(tx*2)))      
+        carpet_segment(n-1, (x2+tx, y2+(tx*2)))
+        carpet_segment(n-1, (x2+(tx*2), y2+(tx*2)))
 
-carpet_segment(3)
+carpet_segment(2)
 
 # plot setup
 plt.summer()
