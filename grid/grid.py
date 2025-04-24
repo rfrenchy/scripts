689c7ef5ca110bb183bd6937f1ed1590d67bef5a
rfrenchy <ryanfrench@outlook.com>
2025-04-24 15:25:36 +0100

    variable input dimension e.g. 5x4

--- grid/grid.py
+++ grid/grid.py
@@ -1,54 +1,57 @@
-# README ME
-
-import numpy as np
 import argparse
+import re
 
-from PIL import Image, ImageDraw, ImageOps
+from PIL import Image, ImageDraw
 
 # command line argument set up
 argp = argparse.ArgumentParser("grid", 
-            description="overlays a 2x4 grid on an image", 
-            usage="grid.py -i ballerina.jpg -o ballerina-grid.jpg")
+            description="overlays a grid on an image", 
+            usage="grid.py -i ballerina.jpg -o ballerina-grid.jpg -x 4x4")
 argp.add_argument(
     "-i", "--input", help="the url of the input image", type=str)
 argp.add_argument("-o", "--output", 
     help="name of output image", type=str)
+argp.add_argument("-x", "--dimensions", 
+    help="dimension string", type=str)
 argp.add_argument("-d", "--debug", help="print debug comments",
 					action=argparse.BooleanOptionalAction)
 
 args = argp.parse_args()
 
-# width 157 add
+wid = 4
+hei = 4
+
+if args.dimensions is not None:
+	dstring = re.search(r'(\d+)x(\d+)', args.dimensions)
+	wid = int(dstring.group(1))
+	hei = int(dstring.group(2))
 
 # open the image for processing
 img = Image.open(args.input)
 
-# drawer
+# Image drawer
 drw = ImageDraw.Draw(img, "RGBA")
 
-# dimensions
+# image dimensions
 w, h = img.size
 if args.debug:
 	print(f"Image width: {w}, height: {h}")
 
-DIVISIONS = 4
-
-dw = w / DIVISIONS
-dh = h / DIVISIONS
-
+# draw line across 'wid' times
+dw = w / wid
 dwx = dw
+
+for i in range(wid):
+	drw.line([(dwx, 0),(dwx, h)], fill=(0,0,50,50), width=16) # down
+	dwx = dwx + dw
+
+# draw line vertical 'hei' times
+dh = h / hei
 dhx = dh
 
-for i in range(DIVISIONS):
-	drw.line([(0, dhx), (w, dhx)], fill=(0,0,50,50), width=16) # across
-	drw.line([(dwx, 0),(dwx, h)], fill=(0,0,50,50), width=16) # down
-	
-	dwx = dwx + dw
+for i in range(hei):
+	drw.line([(0, dhx), (w, dhx)], fill=(0,0,50,50), width=16) 
 	dhx = dhx + dh
 
-# for i in range(DIVISIONS):
-#     wx = w / DIVISIONS * i
-# 		# drw.line([(wx, 0), (wx, h)], fill=(0, 0, 50, 50), width=16)
-
-
+# save output
 img.save(args.output)
