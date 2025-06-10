9c53dcb582f4c04b3d67d44261d0042726fad464
ryan <ryanfrench@outlook.com>
2025-06-10 15:35:01 +0100

    polyhedra add

---
+++ geom/polyhedra.py
@@ -0,0 +1,26 @@
+import vedo
+
+from square_functions import unit_square
+
+# pyvista
+# vedo
+
+# earth
+def cube():
+    # create unit square
+    print("cube")
+
+# fire
+def tetrahedron():
+    print("tetrahedron")
+
+# air
+def octahedron():
+    print("octahedron")
+
+# water
+def icosahedron():
+    print("icosahedron")
+
+def dodecahedron():
+    print("dodecahedron")
