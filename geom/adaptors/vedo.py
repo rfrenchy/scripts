549341940c3274408a588a294870477efd0f7b12
ryan <ryanfrench@outlook.com>
2025-06-11 15:22:58 +0100

    half cube

--- geom/adaptors/vedo.py
+++ geom/adaptors/vedo.py
@@ -1,4 +1,28 @@
-import triangle_functions as triangle
+import numpy as np
+import triangle_functions as tr
 
 def square(scale = 1):
-    return triangle.unit_trianglev3()
+    return tr.unit_trianglev3()
+
+def rotate(cx = 0,cy = 0,cz = 0):
+    rx, ry, rz = np.radians((cx, cy, cz))
+
+    # x rotation matrix
+    Rx = np.array([[1, 0, 0],
+                  [0, np.cos(rx), -np.sin(rx)], 
+                  [0, np.sin(rx), np.cos(rx)]])
+
+    # y rotation matrix
+    Ry = np.array([[np.cos(ry), 0, np.sin(ry)], 
+                  [0, 1, 0], 
+                  [-np.sin(ry), 0, np.cos(ry)]])
+
+    # z rotation matrix
+    Rz = np.array([[np.cos(rz), -np.sin(rz), 0], 
+                  [np.sin(rz), np.cos(rz), 0],
+                  [0, 0, 1]])
+
+    R = Rx @ Ry @ Rz
+    
+    return R
+    
--- geom/polyhedra.py
+++ geom/polyhedra.py
@@ -1,14 +1,17 @@
 import vedo
 import adaptors.vedo
+import numpy as np
 
 # earth
 def cube():
     points = adaptors.vedo.square()
     square = vedo.Line(points, closed=True)
+    points2 = adaptors.vedo.square() @ adaptors.vedo.rotate(90, 0, 0).T
+    square2 = vedo.Line(points2, closed=True)
+    points3 = adaptors.vedo.square() @ adaptors.vedo.rotate(0, -90, 0).T
+    square3 = vedo.Line(points3, closed=True)
 
-    # print(square_functions.unit_square_3d())
-
-    vedo.show(square, axes=1).close()
+    vedo.show(square, square2, square3, axes=1).close()
 
 # fire
 def tetrahedron():
