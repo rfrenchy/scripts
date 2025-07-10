34041717398436193336ada7984711f8d192c3da
ryan <ryanfrench@outlook.com>
2025-07-10 14:15:02 +0100

    rainbow points

--- geom/color.py
+++ geom/color.py
@@ -6,14 +6,22 @@ primary = [
     {"name": "blue", "rgb": (0, 0, 255), "hex": "#0000FF"}, 
 ]
 
+secondary = [
+    {"name": "yellow", "rgb": (255,255,0), "hex": "#FFFF00"},
+    {"name": "orange", "rgb": (255,165,0), "hex": "#FFA500"},
+    {"name": "indigo", "rgb": (75, 0, 130), "hex": "#4B0082"},
+    {"name": "violet", "rgb": (143, 0, 255), "hex": "#8F00FF"}
+]
+
+points = [
+    vedo.Point((255, 255, 255), c="#FFFFFF"), # white
+    vedo.Point((4, 55, 242), c="#0437F2"),    # ultramarine
+    vedo.Point((64, 130, 109), c="#40826D"),  # viridian
+    vedo.Point((138, 51, 36), c="#8A3324"),   # burnt umber
+    vedo.Point((0, 0, 0), c="#000000")        # black
+]
+
 def colors():
-    points = [
-        vedo.Point((255, 255, 255), c="#FFFFFF"), # white
-        vedo.Point((4, 55, 242), c="#0437F2"),    # ultramarine
-        vedo.Point((64, 130, 109), c="#40826D"),  # viridian
-        vedo.Point((138, 51, 36), c="#8A3324"),   # burnt umber
-        vedo.Point((0, 0, 0), c="#000000")        # black
-    ]
 
     pantone = [
         {"name": "cerulean", "rgb": (0, 123, 167), "hex": "#007ba7"}
@@ -21,21 +29,13 @@ def colors():
 
     cer_point = next((p for p in pantone if p["name"] == "cerulean"))
 
-    # create points,
 
-    # create lines
-    cerulean_cluster = []
-#    for c in primary:
+    primary_points = [(vedo.Point(p["rgb"], c=p["hex"])) for p in primary]
+    secondary_points = [(vedo.Point(p["rgb"], c=p["hex"])) for p in secondary]
 
-    # cerulean_cluster = vedo.Line([p["rgb"] for p in cer_points])
-
-#     primary_points = [(vedo.Point(p["rgb"], c=p["hex"])) for p in primary]
-
-
-
-    # vedo.show(points)
-    # vedo.show(primary_points, points, axes=1)
+    vedo.show(primary_points, 
+              secondary_points, 
+              axes=1)
  
- #   vedo.show(cerulean_cluster, primary_points, axes=1)
 
 colors()
